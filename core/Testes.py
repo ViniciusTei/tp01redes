d5274a0ff8f572a194c143f548189dcec1c98a81
viniciusTei <viniciustprates@gmail.com>
2020-11-16 19:21:45 -0300

    Criada simulacoes

--- core/Testes.py
+++ core/Testes.py
@@ -1,13 +1,18 @@
 import random
 import math
+import statistics
 from core.Estacao import Estacao
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 class Testes:
     def Aloha(maquinas, N):
         ativo = []  # maquinas que estao tentando enviar uma mensagem
         canaltempo = 0
-
+        tempoPrimeiraMaquina = 0
+        first = True #
         while(True):
             # verifica se eh o primeiro canal de tempo
             if(canaltempo == 0):
@@ -38,8 +43,11 @@ class Testes:
                         if(m.id == ativo[0].id):
                             m.sending = False
                             m.tempoGasto = canaltempo
-                            print('Maquina: ', m.id)
-                            print('Tempo gasto: ', m.tempoGasto * 51.2, 'us')
+                            if(first):
+                                #print('Maquina: ', m.id)
+                                #print('Tempo gasto: ', m.tempoGasto * 51.2, 'us')
+                                tempoPrimeiraMaquina =  m.tempoGasto
+                                first=False
 
                     # reset dos ativos
                     ativo = []
@@ -49,14 +57,19 @@ class Testes:
 
                     # quando a lista de ativos ficar vazia nao existe mais nenhuma maquina tentando envair uma msg
                     if(len(ativo) == 0):
-                        print('\nTempo total: ', canaltempo * 51.2, 'us')
+                        #print('\nTempo total: ', canaltempo * 51.2, 'us')
+                        
                         break
-
+        return tempoPrimeiraMaquina, canaltempo
+    
     def CSMA1P(maquinas, N):
         ativo = []  # maquinas que estao tentando enviar uma mensagem
         esperandoNoCanal = []  # maquinas que querem transmitir em um determinado canal de tempo
         canaltempo = 0
         p = 1 # probabilidade de 1%
+        first = True #primeira estacao a transmitir
+        tempoPrimeiraMaquina = 0
+
         # comeca a escutar os canais de tempo
         while(True):
             if(canaltempo == 0):
@@ -75,37 +88,43 @@ class Testes:
                     else:
                         m.p += 1
 
+                canaltempo += 1
                 # Verificar as colisoes
                 if(len(ativo) > 1):
                     # houve colisao tratar erro
                     for m in ativo:
-                        m.p = math.floor((random.random()*100) + canaltempo)
+                        m.p = math.floor((random.random()*100)) + canaltempo
                     
-                    ativo.clear()
+                    #ativo.clear()
 
                 elif(len(ativo) == 1):
                     # transmitir
-                    flagEnd = 0
-                    for m in maquinas:
-                        if(m.id == ativo[0].id):
-                            m.sending = False
-                            m.tempoGasto = canaltempo
-                            print('Maquina: ', m.id)
-                            print('Tempo gasto: ', m.tempoGasto * 51.2, 'us')
-                            ativo.clear()
-                            break
-                        elif(m.sending):
-                            flagEnd += 1
+                    for m in ativo:
+                        m.sending = False
+                        m.tempoGasto = canaltempo
+                        if(first):
+                            tempoPrimeiraMaquina = m.tempoGasto
+                            first = False
+                        #print('Maquina: ', m.id)
+                        #print('Tempo gasto: ', m.tempoGasto, 'us')
+                
+                ativo.clear() #sempre limpa a lista de ativos
+                esperandoNoCanal.clear() #sempre limpa a lista das maquinas esperando naquele canal
+                flagEnd = 0
+                for m in maquinas:
+                    if(m.sending):
+                        flagEnd +=1
 
-                    if(flagEnd == 0):
-                        print('\nTempo total: ', canaltempo * 51.2, 'us')
-                        break
-
-                canaltempo += 1
-
+                if(flagEnd == 0):
+                    #print('\nTempo total: ', canaltempo, 'us')
+                    break
+        return tempoPrimeiraMaquina, canaltempo
+    
     def Backoff(maquinas, N):
         ativo = []  # maquinas que estao tentando enviar uma mensagem
         canaltempo = 0
+        first = True #primeira estacao a transmitir
+        tempoPrimeiraMaquina = 0
         # comeca a escutar os canais de tempo
         while(True):
             if(canaltempo == 0):
@@ -133,8 +152,11 @@ class Testes:
                     for m in ativo:
                         m.sending = False
                         m.tempoGasto = canaltempo
-                        print('Maquina: ', m.id)
-                        print('Tempo gasto: ', m.tempoGasto * 51.2, 'us')
+                        if(first):
+                            tempoPrimeiraMaquina = m.tempoGasto
+                            first = False
+                        #print('Maquina: ', m.id)
+                        #print('Tempo gasto: ', m.tempoGasto * 51.2, 'us')
                         break
                     
                     ativo.clear()
@@ -148,6 +170,66 @@ class Testes:
                         coutSending += 1
                 
                 if(coutSending == 0) or (flagEnd == 1):
-                    print('\nTempo total: ', canaltempo * 51.2, 'us')
+                    #print('\nTempo total: ', canaltempo * 51.2, 'us')
                     break
                 
+        return tempoPrimeiraMaquina,canaltempo
+
+    def calculaResultado(tempo, algoritmo, n):
+        tempo_primeira = []
+        tempo_total = []
+
+        #desestruturacao da tupla para facilitar o calculo das medias
+        for t in range(len(tempo)):
+            tempo_primeira.append(tempo[t][0])
+            tempo_total.append(tempo[t][1])
+
+        media_primeira = statistics.mean(tempo_primeira)
+        media_total = statistics.mean(tempo_total)
+        desvio_pirmeiro = statistics.pstdev(tempo_primeira, media_primeira)
+        desvio_total = statistics.pstdev(tempo_total, media_total)
+        print("------- Resultado ---------")
+        print("Algoritmo: ", algoritmo)
+        print("Valor de N: ", n)
+        print("Media primeira estacao: ", media_primeira, " Desvio: ", desvio_pirmeiro)
+        print("Media total: ", media_total, " Desvio: ", desvio_total)
+        print("--------------------------------")
+
+    def executaTestes(N):
+        tempo = []
+        maquinas = []  # lista de maquinas
+        
+        for i in range(0,33):
+            # inicializa lista de maquinas
+            for j in range(N):
+                m = Estacao(j, False, '000000001', 1)
+                maquinas.append(m)
+            tempo.append(Testes.Aloha(maquinas, N))
+            maquinas.clear()
+
+        Testes.calculaResultado(tempo, 'Aloha', N)
+        tempo.clear()
+
+        for i in range(0,33):
+            # inicializa lista de maquinas
+            for j in range(N):
+                m = Estacao(j, False, '000000001', 1)
+                maquinas.append(m)
+            
+            tempo.append(Testes.CSMA1P(maquinas, N))
+            maquinas.clear()
+        
+        Testes.calculaResultado(tempo, 'CSMA-P', N)
+        tempo.clear()
+
+        for i in range(0,33):      
+            # inicializa lista de maquinas
+            for j in range(N):
+                m = Estacao(j, False, '000000001', 1)
+                maquinas.append(m)          
+            
+            tempo.append(Testes.Backoff(maquinas, N))
+            maquinas.clear()
+
+        Testes.calculaResultado(tempo, 'Backoff', N)
+        tempo.clear()
--- main.py
+++ main.py
@@ -1,38 +1,9 @@
 from core.Testes import Testes
-from core.Estacao import Estacao
-
 
 def main():
-
-    maquinas = []  # lista de maquinas
-    N = 20
-    # menu
-    # print("------------------------------\n")
-    # print("   Escolha um valor para N:\n")
-    # print("------------------------------\n")
-    # N = int(input())  # numero de estacoes
-
-    print("------------------------------\n")
-    print("Voce escolheu", N, "estacoes\n")
-    print("Escolha um algoritmo:\n")
-    print("1- Aloha\n")
-    print("2- CSMA p-persistente\n")
-    print("3- Backoff Exponencial Binario\n")
-    print("------------------------------\n")
-    algoritmo = int(input())
-
-    # inicializa lista de maquinas
-    for i in range(N):
-        m = Estacao(i, False, '000000001', 1)
-        maquinas.append(m)
-
-    if (algoritmo == 1):
-        Testes.Aloha(maquinas, N)
-    elif(algoritmo == 2):
-        Testes.CSMA1P(maquinas, N)
-    elif(algoritmo == 3):
-        Testes.Backoff(maquinas, N)
+    Testes.executaTestes(20)
+    Testes.executaTestes(40)
+    Testes.executaTestes(100)
     
-        
 if __name__ == "__main__":
     main()
